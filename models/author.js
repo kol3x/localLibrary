83f10a50d3656a1d31d26db13b99e071d812e8aa
kol3x <kolaegipet@gmail.com>
2023-11-01 18:04:27 +0200

    first com

---
+++ models/author.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+AuthorSchema.virtual("name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.first_name}, ${this.family_name}`;
+  }
+  return fullname;
+});
+
+AuthorSchema.virtual("url").get(function () {
+  return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("birthdate_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
+
+AuthorSchema.virtual("deathdate_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
+
+AuthorSchema.virtual("birth_date_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("death_date_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+router.get('/cool', (req, res, next) => {
+  res.send("you'r so cool");
+});
+
+
+
+module.exports = router;
